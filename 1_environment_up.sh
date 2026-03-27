b0fbccf56d5ab5e319e84e8a83ef85d3dcaf6f9a
Luigi Luz <luigiluz98@gmail.com>
2026-03-27 17:20:11 -0300

    Update scripts

--- 1_environment_up.sh
+++ 1_environment_up.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+CARLA_FOLDER_NAME="${CARLA_FOLDER_NAME:-carla-0-9-15}"
+
 # Start CARLA simulator in the background
 echo "Starting CARLA simulator..."
-./carla/CarlaUE4.sh -RenderOffScreen -quality_level=Low -nosound &
+./${CARLA_FOLDER_NAME}/CarlaUE4.sh -RenderOffScreen -quality_level=Low -nosound &
 
 # Set up virtual CAN bus
 echo "Setting up virtual CAN bus..."
