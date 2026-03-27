7a5e77baf1541e8ace4359ae9f4caaf6aa698ff3
Luigi Luz <luigiluz98@gmail.com>
2026-03-27 17:20:11 -0300

    Update scripts

--- 0_install_dependencies.sh
+++ 0_install_dependencies.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+CARLA_FOLDER_NAME="${CARLA_FOLDER_NAME:-carla-0-9-15}"
+
 echo "Installing "Yes, CARLA CAN" project dependencies..."
 
 echo "Installing can-utils..."
 sudo apt-get install -y can-utils
 
-mkdir -p carla
-cd carla
+mkdir -p ${CARLA_FOLDER_NAME}
+cd ${CARLA_FOLDER_NAME}
 
 echo "Downloading CARLA..."
 wget https://tiny.carla.org/carla-0-9-15-linux
