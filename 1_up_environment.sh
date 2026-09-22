1e0fc1a20fda980d7c9226bd722fa65c2a3cb4f2
Luigi Luz <lfml@cin.ufpe.br>
2026-09-21 22:42:57 -0300

    Fix quality level argument in CARLA startup

--- 1_up_environment.sh
+++ 1_up_environment.sh
@@ -63,7 +63,7 @@ fi
 
 # Start CARLA simulator in the background
 echo "Starting CARLA simulator..."
-./${CARLA_FOLDER_NAME}/CarlaUE4.sh -RenderOffScreen -quality_level=Low -nosound 2>/dev/null &
+./${CARLA_FOLDER_NAME}/CarlaUE4.sh -RenderOffScreen -quality-level=Low -nosound 2>/dev/null &
 
 # Set up virtual CAN bus
 echo "Setting up virtual CAN bus..."
